ec9d72f2e96030658806c84983be57a910240de8
Renzo Bianco <renzobianco222@gmail.com>
2025-05-17 16:05:15 -0300

    Corrijo ruta y config de Vercel

--- firebase.js
+++ firebase.js
@@ -4,11 +4,11 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Configuración de Firebase
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBvzwAiEwWds_WnC_5Gfk_-ZN6PGfMrmD4",
   authDomain: "multiagente-login.firebaseapp.com",
   projectId: "multiagente-login",
-  storageBucket: "multiagente-login.appspot.com", // Corrige el dominio del storage
+  storageBucket: "multiagente-login.appspot.com",
   messagingSenderId: "298638656074",
   appId: "1:298638656074:web:f4610a9dbbde82ea409cb3",
   measurementId: "G-86LQC6LLPD"
