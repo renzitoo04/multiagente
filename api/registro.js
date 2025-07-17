7da6e85439623ec610509fdd8d5812bb988eccc1
Renzo Bianco <renzobianco222@gmail.com>
2025-07-16 23:36:41 -0300

    cambio en texto o imagen

--- api/registro.js
+++ api/registro.js
@@ -1,5 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
+console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
+console.log('SUPABASE_KEY:', process.env.SUPABASE_KEY);
+
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 export default async function handler(req, res) {
