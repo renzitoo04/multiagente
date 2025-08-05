b8cf980806f9b318da8d0548ab1ef6ee4cd3594c
Renzo Bianco <renzobianco222@gmail.com>
2025-08-05 11:24:27 -0300

    Actualización de números editables

--- api/generar-link.js
+++ api/generar-link.js
@@ -2,8 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import mercadopago from 'mercadopago';
-import dotenv from 'dotenv';
-dotenv.config();
+
 
 mercadopago.configure({
   access_token: process.env.MERCADO_PAGO_TOKEN
